5a06b795c5bc81bac60ccbbb18423c849f517bf6
Rafael Guimaraes <raf.plguimaraes@gmail.com>
2023-09-01 17:10:10 -0300

    feat!: Adjusted index.html so it doesnt have any pizza already written, corrected images sizes throughout the application and add function to take picture and finished flow to create or update pizza through the same button

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('deviceready', onDeviceReady, false);
 var listaPizzasCadastradas = [];
+var pizza = null;
 
 function atualizarLista() {
     cordova.plugin.http.get('https://pedidos-pizzaria.glitch.me/admin/pizzas/pizzaria_142317',
@@ -9,17 +10,14 @@ function atualizarLista() {
     function (response) {
         listaPizzasCadastradas = JSON.parse(response.data);
         listaPizzas.innerHTML = '';
-        listaPizzasCadastradas.forEach(pizza => {
-            listaPizzas.innerHTML += `
-            <li id="${pizza._id}">
-                <h2>${pizza.pizza}</h2>
-                <h3>R$ ${pizza.preco}</h3>
-                <img style="display:none" src="${pizza.imagem}" alt="${pizza.pizza}"></img>
-            </li>
-            `;
-        });
-        document.getElementsByClassName('li').forEach(li => {
-            alert(li);
+        listaPizzasCadastradas.forEach((item, idx) => {
+            const novo = document.createElement('li');
+                        novo.innerHTML = `<h2>${item.pizza}</h2><h3>R$ ${item.preco}</h3><figure><img src="${item.imagem}" alt=""></figure>`;
+            novo.id = idx;
+            novo.onclick = function () {
+                carregarDadosPizza (novo.id);
+            };
+            listaPizzas.appendChild(novo);
         });
     },
     function (response) {
@@ -37,46 +35,88 @@ function btnNovoFunc() {
 }
 
 function btnSalvarFunc() {
-    let pizza = document.getElementById('pizza').value;
+    let pizzaNome = document.getElementById('pizza').value;
     let preco = document.getElementById('preco').value;
     let imagem = 'https://i.imgur.com/fCQHvUP.gif';
+    let pizzaObj = {};
+    if (pizza) {
+        pizzaObj = {
+            pizzaid: pizza._id,
+            pizzaria: pizza.pizzaria,
+            pizza: pizzaNome,
+            preco: preco,
+            imagem: imagem
+        };
+        cordova.plugin.http.put('https://pedidos-pizzaria.glitch.me/admin/pizza/',
+        pizzaObj,
+        {},
+        function (response) {
+            alert('Pizza atualizada com sucesso!')
+            atualizarLista();
+            btnCancelarFunc();
+        },
+        function (response) {
+            console.error(response.error);
+        });
+    } else {
+        pizzaObj = {
+            pizzaria: 'pizzaria_142317',
+            pizza: pizzaNome,
+            preco: preco,
+            imagem: imagem
+        };
+    
+        cordova.plugin.http.post('https://pedidos-pizzaria.glitch.me/admin/pizza/',
+        pizzaObj,
+        {},
+        function (response) {
+            alert('Pizza cadastrada com sucesso!')
+            atualizarLista();
+            btnCancelarFunc();
+        },
+        function (response) {
+            console.error(response.error);
+        });
+    }
 
-    let pizzaObj = {
-        pizzaria: 'pizzaria_142317',
-        pizza: pizza,
-        preco: preco,
-        imagem: imagem
-    };
-
-    cordova.plugin.http.post('https://pedidos-pizzaria.glitch.me/admin/pizza/',
-    pizzaObj,
-    {},
-    function (response) {
-        atualizarLista();
-        btnCancelarFunc();
-    },
-    function (response) {
-        console.error(response.error);
-    });
+    
 }
 
 function btnCancelarFunc() {
     applist.style.display = 'flex'; // exibe lista
     appcadastro.style.display = 'none'; // oculta cadastro
+    pizza = null;
 }
 
 function btnExcluirFunc() {
-// cordova.plugin.http.delete(`https://pedidos-pizzaria.glitch.me/admin/pizza/pizzaria_142317/arrroz`,
-    // {},
-    // {},
-    // function(response){
-    //     console.log(response.data)
-    // },
-    // function(response){
-    //     console.log(response.error)
-// });
+    cordova.plugin.http.delete(`https://pedidos-pizzaria.glitch.me/admin/pizza/pizzaria_142317/${pizza.pizza}`,
+        {},
+        {},
+        function(response){
+            console.log(response.data)
+        },
+        function(response){
+            console.log(response.error)
+    });
+    alert('Pizza excluída com sucesso!')
+    atualizarLista();
+    btnCancelarFunc();
 }
 
+function btnFotoFunc() {
+    alert('Foto adicionada com sucesso!');
+    document.getElementById('imagem').src = 'https://i.imgur.com/fCQHvUP.gif';
+}
+
+function carregarDadosPizza (id) {
+    pizza = listaPizzasCadastradas[id];
+    applist.style.display = 'none'; // oculta lista
+    appcadastro.style.display = 'flex'; // exibe cadastro
+    document.getElementById('pizza').value = `${pizza.pizza}`;
+    document.getElementById('preco').value = `${pizza.preco}`;
+    document.getElementById('imagem').src = `${pizza.imagem}`;
+    btnExcluir.style.display = 'block';
+}
 
 function onDeviceReady() {
     cordova.plugin.http.setDataSerializer('json');
@@ -92,11 +132,11 @@ function onDeviceReady() {
 
     atualizarLista();
 
-
     btnNovo.addEventListener('click', btnNovoFunc);
     btnSalvar.addEventListener('click', btnSalvarFunc);
     btnCancelar.addEventListener('click', btnCancelarFunc);
     btnExcluir.addEventListener('click', btnExcluirFunc);
+    btnFoto.addEventListener('click', btnFotoFunc);
 
     document.getElementById('deviceready').classList.add('ready');
 }
